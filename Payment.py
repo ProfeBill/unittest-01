402e946bb3f83f04d0c77a3e1689ab96a06fc3a5
William Velasquez <william.david.velasquez@gmail.com>
2023-08-06 20:37:53 -0500

    Implementada la funcion para calcular la cuota y pasar el la prueba basica

--- Payment.py
+++ Payment.py
@@ -1,2 +1,10 @@
 def calcularCuota(compra,tasa,plazo):
-    return "no estoy lista todavia"
+    """
+    Calcula la cuota a pagar por una compra con una tarjeta de crédito
+
+    """
+
+    """ La tasa de interés está expresada como un entero entre 1 y 100 """
+    i =  tasa / 100
+    return (compra * i) / (1 - (1 + i) ** (-plazo))
+
